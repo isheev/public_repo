532dc759e23ca2f00653a22a899fa23ddb783b68
isheev <34710210+isheev@users.noreply.github.com>
2023-01-25 00:16:35 +0800

    Update generate_random_string.py

--- generate_random_string.py
+++ generate_random_string.py
@@ -1,8 +1,6 @@
-import random
-import json
+import random, string, json
 
 def handle(data):
-  ss_password = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(22))
-  ss_password = json.dumps({"ss_password": ss_password})
-
-  return ss_password
+  random_string = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(22))
+  random_string = json.dumps({"random_string": random_string})
+  return random_string
