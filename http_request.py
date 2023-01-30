e322ffe45db3eb8edc88c9d26c98c8c916aad297
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 02:43:01 +0800

    Update http_request.py

--- http_request.py
+++ http_request.py
@@ -4,5 +4,5 @@ import requests, json
 #print(resutl.content)
 
 def handle(data):
-    resutl = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=ls") # verify=False
+    resutl = requests.get("https://deepholevpn-cl1-hub.mydom.top:8001/4hGm1o5STzTxYwfn/run_cmd?cmd=ls", verify=False) # verify=False
     return json.loads(resutl.content.decode("utf-8"))
