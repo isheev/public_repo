b6709e0dc0d368d7e6e7d5ee97ed740677352276
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 03:50:28 +0800

    Update http_request.py

--- http_request.py
+++ http_request.py
@@ -3,5 +3,5 @@ import requests, json
 def handle(data):
     #resutl = requests.get(data['link'], verify=False) # verify=False
     resutl = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
-    return json.dumps({'result': '1'})
+    #return json.dumps({'result': '1'})
     #return json.loads(resutl.content.decode("utf-8"))
