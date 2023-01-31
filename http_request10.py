1f25d537277319f3672e2746a46014ff57bb5267
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 14:36:22 +0800

    Update and rename http_request10.py to http_request11.py

--- http_request10.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-def handle(data):
-    resutl = requests.get(data['link']) # verify=False
-    #resutl = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
-    #return json.dumps({"test": "2"})
-    return str(json.dumps({"params": data, "version": "10"}))
-    #return json.loads(resutl.content.decode("utf-8"))
