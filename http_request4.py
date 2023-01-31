7e5dcac19fd74725f982bed22e9ef102f9c6d994
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 14:18:49 +0800

    Update and rename http_request3.py to http_request4.py

--- http_request4.py
+++ http_request4.py
@@ -4,5 +4,5 @@ def handle(data):
     resutl = requests.get(data['link'], verify=False) # verify=False
     #resutl = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
     #return json.dumps({"test": "2"})
-    return json.dumps({"params": str(data), "version": "3"})
+    return json.dumps({"params": str(data), "version": "4"})
     #return json.loads(resutl.content.decode("utf-8"))
