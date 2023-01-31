ca9136217b584046e49e767a5d0963f3bfa07eaf
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 15:09:44 +0800

    Update and rename http_request18.py to http_request_v19.py

--- http_request_v19.py
+++ http_request_v19.py
@@ -5,5 +5,5 @@ def handle(data): # data come as string
     result = requests.get(params_json['link'], verify=False) # verify=False
     #result = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
     #return json.dumps({"test": "2"})
-    return json.dumps({"params": params_json, "version": 18, "response": json.loads(result.content.decode("utf-8"))})
+    return json.dumps({"script_ver": 19, "params": params_json, "response": json.loads(result.content.decode("utf-8"))})
     #return json.loads(result.content.decode("utf-8"))
