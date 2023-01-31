246d305333329f6cd3245c4de1d03b94dadbd1c2
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 14:56:41 +0800

    Update and rename http_request16.py to http_request17.py

--- http_request17.py
+++ http_request17.py
@@ -1,9 +1,9 @@
 import requests, json
 
 def handle(data): # data come as string 
-    data_json = json.loads(data)
-    result = requests.get(data_json['link'], verify=False) # verify=False
+    params_json = json.loads(data)
+    result = requests.get(params_json['link'], verify=False) # verify=False
     #result = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
     #return json.dumps({"test": "2"})
-    return str(json.dumps({"params": json.loads(data), "version": "16", "response": json.loads(result.content.decode("utf-8"))}))
+    return str(json.dumps({"params": params_json, "version": 17, "response": json.loads(result.content.decode("utf-8"))}))
     #return json.loads(result.content.decode("utf-8"))
