a1b3982f22f2e424e82ed919776a9b01027a6d61
isheev <34710210+isheev@users.noreply.github.com>
2023-01-31 14:17:07 +0800

    Update and rename http_request2.py to http_request3.py

--- http_request3.py
+++ http_request3.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def handle(data):
-    #resutl = requests.get(data['link'], verify=False) # verify=False
+    resutl = requests.get(data['link'], verify=False) # verify=False
     #resutl = requests.get("https://vpnff-hub.mydom.top:8443/4hGm1o5STzTxYwfn/run_cmd?cmd=hostname", verify=False) # verify=False
     #return json.dumps({"test": "2"})
-    return json.dumps({"params": data, "response": "2"})
+    return json.dumps({"params": data, "version": "3", "response": resutl.content.decode("utf-8")})
     #return json.loads(resutl.content.decode("utf-8"))
